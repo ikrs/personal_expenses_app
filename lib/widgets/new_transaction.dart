8e77d8d29ab7b7142727d0d0b5f80ba738130def
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-18 17:27:39 +0100

    added date picker

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
@@ -10,8 +12,9 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +26,48 @@ class _NewTransactionState extends State<NewTransaction> {
           crossAxisAlignment: CrossAxisAlignment.end, // button to the right
           children: <Widget>[
             TextField(
-              controller: titleController,
+              controller: _titleController,
               // flutter automatically connects controllers to our text inputs and this
               //   controllers listen to user inputs and save the user input
               decoration: InputDecoration(labelText: 'Title'),
-              onSubmitted: (_) => submitData(),
+              onSubmitted: (_) => _submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
+              controller: _amountController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
+              onSubmitted: (_) => _submitData(),
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  // expanded takes as much free space as it can get
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No date choosen!'
+                          : 'Picked Date : ${DateFormat('d.MM.y').format(_selectedDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: _presentDatePicker,
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
               child: Text('Add Transaction'),
-              textColor: Colors.purple,
-              onPressed: submitData,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
+              onPressed: _submitData,
             ),
           ],
         ),
@@ -46,10 +75,10 @@ class _NewTransactionState extends State<NewTransaction> {
     );
   }
 
-  void submitData() {
-    final enteredTitle = titleController.text;
+  void _submitData() {
+    final enteredTitle = _titleController.text;
     final enteredAmount =
-        double.parse(amountController.text); // text to double/float
+        double.parse(_amountController.text); // text to double/float
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
@@ -60,5 +89,22 @@ class _NewTransactionState extends State<NewTransaction> {
 
     // close model popup after submit
     Navigator.of(context).pop();
+  }
+
+  // implementing date picker
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 }
